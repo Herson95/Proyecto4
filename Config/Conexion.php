010765c42ee0e3264069ef9f142d356c266b7aa8
Herson95 <hostinmarvish@gmail.com>
2018-07-22 23:16:05 -0600

    Conexion

--- Config/Conexion.php
+++ Config/Conexion.php
@@ -4,15 +4,19 @@ function Conexion(){
     $user = "root";
     $pass = "";
     $db = "bd_agencia";
+    try{
+        $cnx = mysqli_connect($server,$user,$pass,$db);
+       /* if(!$cnx){
+            die ("ERROR EN LA CONEXION CON MYSQL : " . mysqli_error());
+        }
 
-    $cnx = mysqli_connect($server,$user,$pass,$db);
-    if(!$cnx){
-        die ("ERROR EN LA CONEXION CON MYSQL : " . mysqli_error());
+        $base = mysqli_select_db($cnx,$db);
+        if(!$base){
+            die ("ERROR EN LA CONEXION CON LA BASE DE DATOS : " . mysqli_error($cnx));
+        }*/
     }
-
-    $base = mysqli_select_db($cnx,$db);
-    if(!$base){
-        die ("ERROR EN LA CONEXION CON LA BASE DE DATOS : " . mysqli_error($cnx));
+    catch(mysqli_sql_excepcion $e){
+        throw $e->getMessage();
     }
     return $cnx;
 }
